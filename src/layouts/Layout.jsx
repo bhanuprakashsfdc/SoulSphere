1b5a694a9214d4bdce3ae081308aefb4978cbc35
bhanuprakashsfdc <bhanusunny7@gmail.com>
2025-02-05 19:56:04 +0530

    pushing home design

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom';
+import MiniHeader from '../components/MiniHeader';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import MiniFooter from '../components/MiniFooter';
 
 const Layout = () => {
   return (
     <div className="layout">
+      <MiniHeader />
+      <Header />
       <main className="main" id="top">
         <Outlet />
-      </main>      
+      </main>    
+      <Footer />
+      <MiniFooter />  
     </div>
   )
 }
